ca7ad165f5b8d833588d2ebcf50df0164d9bcd63
gkp2000 <91838051+gkp2000@users.noreply.github.com>
2021-11-21 10:42:17 +0400

    first

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+
+//creating app
+const app = express();
+
+//send an HTTP response when receiving HTTP GET /
+app.use(express.static('public'));
+app.get('/', (req, res) => {
+ res.sendfile('index.html', { root: __dirname });
+});
+
+//make the app listen on port
+const port = process.argv[2] || process.env.PORT || 3000;
+const server = app.listen(port, () => {
+ console.log(`Cart app listening at http://localhost:${port}`);
+});
+
+//handling static HTML and EJS templates
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => {
+ res.render('index'); //no need for ejs extension
+});
+
+//route for contacts
+app.get('/contacts', (req, res) => {
+    res.render('contacts');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+// using JSON and URL Encoded middleware 
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true })); 
